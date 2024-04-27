31b5542fe07efe730105d322fbee88fbbac6a899
Charles Swaleh <charlswall@live.com>
2024-04-27 22:11:33 +0300

    added the API routes to view, add and delete resources

--- jambopay/views.py
+++ jambopay/views.py
@@ -1,6 +1,10 @@
 from django.http import JsonResponse
 from django.forms.models import model_to_dict
 from .models import Customer, Business
+from .forms import CustomerForm, BusinessForm
+from django.views.decorators.http import require_http_methods
+from django.views.decorators.csrf import csrf_exempt
+import json
 
 # Create your views here.
 
@@ -13,44 +17,96 @@ def index(request):
                          "business": "/businesses/id"
                          }})
 
+#exempt the csrf only because I am testing with POSTMAN, otherwise, do not do this
+@csrf_exempt
+@require_http_methods(["GET", "POST"])
 def customers(request):
   """
-  Return a list of all customers currently registered
+  Return a list of all customers currently registered or create a new customer
   """
-  customers = Customer.objects.all()
-  return JsonResponse({"customers": [model_to_dict(customer) for customer in customers]})
+  if request.method == "GET":
+    customers = Customer.objects.all()
+    return JsonResponse({"customers": [model_to_dict(customer) for customer in customers]})
+  elif request.method == "POST":
+    data = request.POST
+    form = CustomerForm(data)
+    print(form)
+    if form.is_valid():
+      customer = form.save()
+      return JsonResponse(model_to_dict(customer), status=201)
+    else:
+        return JsonResponse(form.errors, status=400)
 
+
+
+#exempt the csrf only because I am testing with POSTMAN, otherwise, do not do this
+@csrf_exempt
+@require_http_methods(["GET", "PATCH", "DELETE"])
 def customer(request, pk):
   """
-  Return the details of a single customer by their primary_key
+  Return, edit or delete the details of a single customer by their primary_key
   """
   try:
     customer = Customer.objects.get(pk=pk)
-    return JsonResponse(model_to_dict(customer))
   except Customer.DoesNotExist:
     return JsonResponse({"error": "Customer not found!"}, status=404)
 
+  if request.method == "GET":
+    return JsonResponse(model_to_dict(customer))
+  elif request.method == "DELETE":
+    customer.delete()
+    return JsonResponse({"message": "Customer deleted"}, status=204)
+
+
+#exempt the csrf only because I am testing with POSTMAN, otherwise, do not do this
+@csrf_exempt
+@require_http_methods(["GET", "POST"])
 def businesses(request):
   """
-  Returns a list of all businesses and their related owners
+  Returns a list of all businesses and their related owners or creates a new business
   """
-  businesses = Business.objects.select_related('owner').all()
-  businesses_data = [{'name': business.name,
-                      'owner': business.owner.name,
-                      'category': business.category,
-                      'registration_date': business.registration_date,
-                      'business_age': business.business_age} for business in businesses]
-  return JsonResponse({'businesses': businesses_data}, safe=False)
+  if request.method == "GET":
+    businesses = Business.objects.select_related("owner").all()
+    businesses_data = [{"id": business.id,
+                        "name": business.name,
+                        "owner": business.owner.name,
+                        "category": business.category,
+                        "registration_date": business.registration_date,
+                        "business_age": business.business_age} for business in businesses]
+    return JsonResponse({"businesses": businesses_data}, safe=False)
+  elif request.method == "POST":
+    data = request.POST
+    form = BusinessForm(data)
+    if form.is_valid():
+        business = form.save()
+        return JsonResponse({"id": business.id,
+                        "name": business.name,
+                        "owner": business.owner.name,
+                        "category": business.category,
+                        "registration_date": business.registration_date,
+                        "business_age": business.business_age}, status=201)
+    else:
+        return JsonResponse(form.errors, status=400)
 
+
+#exempt the csrf only because I am testing with POSTMAN, otherwise, do not do this
+@csrf_exempt
+@require_http_methods(["GET", "DELETE"])
 def business(request, pk):
   """
   Returns a the details of a single business, including the owner
   """
   try:
-      business = Business.objects.select_related('owner').get(pk=int(pk))
-      business_data = model_to_dict(business)
-      business_data['owner'] = business.owner.name
-      business_data['business_age'] = business.business_age
-      return JsonResponse(business_data)
-  except (Business.DoesNotExist, ValueError):
-      return JsonResponse({'error': 'Business not found'}, status=404)
+    business = Business.objects.select_related("owner").get(pk=int(pk))
+  except Business.DoesNotExist:
+    return JsonResponse({"error": "Business not found"}, status=404)
+
+  if request.method == "GET":
+    business_data = model_to_dict(business)
+    business_data["owner"] = business.owner.name
+    business_data["business_age"] = business.business_age
+    return JsonResponse(business_data)
+  elif request.method == "DELETE":
+    business.delete()
+    return JsonResponse({"message": "Business deleted"}, status=204)
+
